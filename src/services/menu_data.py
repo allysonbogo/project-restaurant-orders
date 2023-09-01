967b3e625736a9af9693dbbc435570154c376c75
Allyson Bogo <ab.bogo@gmail.com>
2023-09-01 16:59:52 -0300

    implements MenuData class

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,37 @@
-# Req 3
+from models.ingredient import Ingredient
+from models.dish import Dish
+import csv
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.load(source_path)
+        self.add_ingredients(source_path)
+
+    def __iter__(self):
+        return iter(self.dishes)
+
+    def __str__(self) -> str:
+        return f"{self.dishes}"
+
+    def load(self, source_path: str) -> None:
+        with open(source_path, "r") as file:
+            reader = csv.reader(file)
+            next(reader)
+
+            for row in reader:
+                dish = Dish(row[0], float(row[1]))
+                self.dishes.add(dish)
+
+    def add_ingredients(self, source_path):
+        with open(source_path, "r") as file:
+            reader = csv.reader(file)
+            next(reader)
+
+            for row in reader:
+                for dish in self.dishes:
+                    if dish == Dish(row[0], float(row[1])):
+                        dish.add_ingredient_dependency(
+                            Ingredient(row[2]), int(row[3])
+                        )
