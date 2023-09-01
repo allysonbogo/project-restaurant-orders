89cdd02a610d94dec11391e8c2007eb7f736cc9a
Allyson Bogo <ab.bogo@gmail.com>
2023-09-01 14:45:56 -0300

    implements tests for Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,30 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ingredient_1 = Ingredient("bacon")
+    ingredient_2 = Ingredient("massa de lasanha")
+
+    assert ingredient_1.name == "bacon"
+    assert ingredient_2.name == "massa de lasanha"
+
+    assert ingredient_1.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+    assert ingredient_2.restrictions == {
+        Restriction.GLUTEN,
+        Restriction.LACTOSE,
+    }
+
+    assert hash(ingredient_1) == hash("bacon")
+    assert hash(ingredient_2) == hash("massa de lasanha")
+
+    assert ingredient_1.__eq__(ingredient_1) is True
+    assert ingredient_1.__eq__(ingredient_2) is False
+    assert ingredient_1 == ingredient_1
+    assert ingredient_1 != ingredient_2
+
+    assert repr(ingredient_1) == "Ingredient('bacon')"
+    assert repr(ingredient_2) == "Ingredient('massa de lasanha')"
